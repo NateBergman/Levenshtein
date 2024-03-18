a5b5bb2dcdf79783023eb848535967b870e6aaaa
NateBergman <90053358+NateBergman@users.noreply.github.com>
2024-03-18 10:40:38 -0700

    Pretty sure this is the final version for now. Both sides (file or not) is super fast.

--- src/BothSideNoFile.java
+++ src/BothSideNoFile.java
@@ -1,19 +1,26 @@
 import java.io.*; //Levenshtein by Nate Bergman
 import java.util.*;
-public class BothSideWithFile {
+public class BothSideNoFile {
     public static void main (String[] args) throws FileNotFoundException {
-        Map<String,Set<String>> moveMap = new TreeMap<>(); //builds map of valid moves for each word from file
-        Scanner lineScanner = new Scanner(new File("src/Moves"));
-        while (lineScanner.hasNext()) { //slowest part by far, should probably clean up
-            Scanner wordScanner = new Scanner(lineScanner.nextLine());
-            String key = wordScanner.next();
-            Set<String> adjacents = new HashSet<String>();
-            wordScanner.useDelimiter(", |]| "); //I can't figure out how to use [ in the delimeter
-            adjacents.add(wordScanner.next().substring(1)); //so I had to do this :(
-            while (wordScanner.hasNext()) {
-                adjacents.add(wordScanner.next());
+        ArrayList<String> words = new ArrayList<String>(); //list of all words
+        int[] lengths = new int[36]; //sores indexes of where the lengths change
+        Arrays.fill(lengths,-1);
+        Scanner dictionary = new Scanner(new File("src/DictionarySortedByLength"));
+        while (dictionary.hasNext()) {
+            words.add(dictionary.next().toLowerCase());
+        }
+        int longest = words.get(words.size() - 1).length(); //build map of what index each length is at
+        lengths[words.get(0).length()] = 0;
+        for (int i = 1; i < words.size(); i++) {
+            if (words.get(i).length() > words.get(i - 1).length()) {
+                lengths[words.get(i).length()] = i;
             }
-            moveMap.put(key,adjacents);
+        }
+        lengths[longest + 2] = words.size();
+        for (int i = longest + 1; i > -1; i--) {
+            if (lengths[i] == -1) {
+                lengths[i] = lengths[i + 1];
+            }
         }
 
         Map<String,Set<String>> previous = new HashMap<>(); //shows what words can lead to this one in a shortest path
@@ -48,7 +55,7 @@ public class BothSideWithFile {
                         prevDepth2--;
                         break;
                     }
-                    Set<String> moves = moveMap.get(word);
+                    Set<String> moves = getAdjacents(word,words,lengths);
                     for (String s : moves) {
                         if (!depths.containsKey(s)) { //unexplored words
                             previous.put(s, new HashSet<>());
@@ -75,7 +82,7 @@ public class BothSideWithFile {
                         prevDepth1++;
                         break;
                     }
-                    Set<String> moves = moveMap.get(word);
+                    Set<String> moves = getAdjacents(word,words,lengths);
                     for (String s : moves) {
                         if (!depths.containsKey(s)) { //unexplored words
                             previous.put(s, new HashSet<>());
@@ -116,5 +123,50 @@ public class BothSideWithFile {
             }
             return pathCount;
         }
+    }
+    public static Set<String> getAdjacents (String word, ArrayList<String> words, int[] lengths) { //returns all valid moves
+        Set<String> moves = new HashSet<>();
+        for (int i = lengths[word.length() - 1]; i < lengths[word.length() + 2]; i++) {
+            String newWord = words.get(i); //goes through every word same length, 1 shorter, or 1 longer and...
+            if (!word.equals(newWord) && isAdjacent(newWord,word)) { //tests if they are one character away and if so...
+                moves.add(words.get(i)); //adds them to the list of legal moves
+            }
+        }
+        return moves;
+    }
+    public static boolean isAdjacent (String s1, String s2) { //CAN ONLY CALL ON WORDS SAME SIZE OR 1 SIZE DIFFERENT
+        if (s1.length() == s2.length()) {//if the words are same length it's simple, just comapre each and if there's >1 mismatch they don't work
+            boolean mismatch = false;
+            int l = s1.length();
+            for (int i = 0; i < l; i++) {
+                if (s1.charAt(i) != s2.charAt(i)) {
+                    if (mismatch) {
+                        return false;
+                    } else {
+                        mismatch = true;
+                    }
+                }
+            }
+        } else {
+            if (s1.length() < s2.length()) { //1 is always longer, so if 2 input is need to flip them.
+                String s3 = s2;
+                s2 = s1;
+                s1 = s3;
+            }
+            int i = 0;
+            int j = 0;
+            int l1 = s1.length();
+            int l2 = s2.length();
+            while (j < l2) { //algorithm increments both words if characters are the same, only longer word if they're different
+                if (i == l1) { //assumes words are only one off and so if longer finishes before smaller there's 2+ mismatches
+                    return false;
+                }
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    j++;
+                }
+                i++;
+            }
+        }
+        return true; //if not enough mismatches then they work
     }
 }
